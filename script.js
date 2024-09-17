e7ab992a31db2a56118dc15d40ded4954292a7f3
lucykronberg <lucykronberg@gmail.com>
2024-09-17 13:09:01 -0700

    wellness website

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+const movies = ["The Godfather","My Little Pony", "Barbie", "Mulan", "Alien", "Scream", "Avatar", "2001: A Space Odyssey", "Fried Green Tomatoes", "Shrek", "How to Train Your Dragon","Matilda","Iron Man","Baby Driver","Annie", "Shawshank Redemption","Star Wars","Wall-E","Tangled"];
+const books = ["The Hunger Games", "Percy Jackson", "Harry Potter", "The Color Purple", "Animal Farm", "Twilight", "I'm Glad My Mom Died", "A Dog's Purpose", "It Ends With Us", "Anne of Green Gables", "Fourth Wing", "Pride & Prejudice", "Coraline", "Pet Semetary", "It", "Daisy Jones & The Six", "The Life of Pi","The Hate U Give", "Romeo & Juliet"];
+
+
+
 $(document).ready(function() {
 	$("#guidedvid").click(function(){
 		$(this).next().toggle();
 	});
-<<<<<<< HEAD
+
 	$(".pageLinks").click(function(){
 		$(this).next().toggle();
 	});
@@ -13,7 +18,6 @@ $(document).ready(function() {
 	$("#buttonB").click(function() {
 		$("#randomB").text(books[Math.floor(Math.random() * books.length)]);
 	});
-=======
+
 	
->>>>>>> 5cb675c4bd8368dfbc6cf517e2785ef0bbd8a361
 });
